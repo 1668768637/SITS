fdd68dc8ce0d38bfbb34e5fdec27dd5dad9f90e4
hb <1668768637@qq.com>
2023-01-31 15:14:46 +0800

    reform webUI with bootstrap the postImages have some problems about layout when nums of images are not range in rules escpeilly ruled num add one combine home.right-main-contianer and forum.right-main-contianer into one and save in base.html

--- forum/views.py
+++ forum/views.py
@@ -4,39 +4,23 @@ from django.http import JsonResponse
 
 # Create your views here.
 
-def anecdoteForum(request):
-    postList = Post.objects.all().filter(postType="anecdoteForum").order_by("likesNum")
-    return render(request,'anecdoteForum.html',{'active_menu':'anecdoteForum','postList':postList})
-
-def lostAndFound(request):
-    return render(request,'lostAndFound.html',{'active_menu':'lostAndFound'})
-
-def driftBottle(request):
-    return render(request,'driftBottle.html',{'active_menu':'driftBottle'})
-
-def eventInformation(request):
-    return render(request,'eventInformation.html',{'active_menu':'eventInformation'})
-    
-def loveWall(request):
-    return render(request,'loveWall.html',{'active_menu':'loveWall'})
-
 def forum(request,postType):
     menu = postType
     postList = []
     if postType == "anecdoteForum":
-        postList = Post.objects.all().filter(postType="anecdoteForum").order_by("likesNum")
+        postList = Post.objects.all().filter(postType="anecdoteForum").order_by("-likesNum")
         postType = "校园轶事"
     if postType == "lostAndFound":
-        postList = Post.objects.all().filter(postType="lostAndFound").order_by("likesNum")
+        postList = Post.objects.all().filter(postType="lostAndFound").order_by("-likesNum")
         postType = "失物招领"
     if postType == "driftBottle":
-        postList = Post.objects.all().filter(postType="driftBottle").order_by("likesNum")
+        postList = Post.objects.all().filter(postType="driftBottle").order_by("-likesNum")
         postType = "漂流瓶"
     if postType == "eventInformation":
-        postList = Post.objects.all().filter(postType="eventInformation").order_by("likesNum")
+        postList = Post.objects.all().filter(postType="eventInformation").order_by("-likesNum")
         postType = "活动信息"
     if postType == "loveWall":
-        postList = Post.objects.all().filter(postType="loveWall").order_by("likesNum")
+        postList = Post.objects.all().filter(postType="loveWall").order_by("-likesNum")
         postType = "表白墙"
     return render(request,'forum.html',{'active_menu':menu,'postList':postList,'postType':postType})
 
